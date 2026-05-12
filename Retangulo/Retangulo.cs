b83f8dc709157bcfd493110c465a84b9ff0b2f97
EneiasS <joaovictorenea25@gmail.com>
2026-05-12 08:13:54 -0300

    Update Retangulo.cs

--- Retangulo/Retangulo.cs
+++ Retangulo/Retangulo.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre com a Largura e Altura o Retangulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PARIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+
 
